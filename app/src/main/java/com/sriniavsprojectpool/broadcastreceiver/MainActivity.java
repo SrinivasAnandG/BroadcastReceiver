c8b25c6b781f50b81aab52d2e4f889feec9e6955
srinivasanand <srinivasgooduri49@gmail.com>
2019-06-03 17:46:33 +0530

    initial setup project

---
+++ app/src/main/java/com/sriniavsprojectpool/broadcastreceiver/MainActivity.java
@@ -0,0 +1,34 @@
+package com.sriniavsprojectpool.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+
+import com.sriniavsprojectpool.broadcastreceiver.BroadcastFiles.BraodcastSample;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        BroadcastReceiver broadcastReceiver = new BraodcastSample();
+//        IntentFilter intentFilter = new IntentFilter(getPackageName().toString());
+//        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+//        this.registerReceiver(broadcastReceiver,intentFilter);
+//    }
+}
